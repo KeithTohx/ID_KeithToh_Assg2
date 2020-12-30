c2a6de58bb2919571a24a4abe14828c7dd368bad
KeithTohx <keith2703@icloud.com>
2020-12-30 15:40:03 +0800

    added check for weather (rainfall value) feature

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let markersArray = [];
+let markersArrayTraf = [];
+let markersArrayRain = [];
 
 function initMap() {
   // map options
@@ -9,55 +10,57 @@ function initMap() {
   // new map
   var map = new google.maps.Map(document.getElementById('map'), options);
 
+
+  // SG TRAFFIC IMAGES
   // Adds a marker to the map and push to the array
-  function addMarker(props){
-    var marker = new google.maps.Marker({
+  function addMarkerTraf(props){
+    var markerTraf = new google.maps.Marker({
       position: props.coords,
       map: map
     });
-    markersArray.push(marker);
+    markersArrayTraf.push(markerTraf);
 
     // check for custom icon
-    if (props.iconImage){
-      marker.setIcon(props.iconImage);
+    if (props.iconImageTraf){
+      markerTraf.setIcon(props.iconImageTraf);
     }
 
     // check content
     if (props.content){
-      let infoWindow = new google.maps.InfoWindow({
+      let infoWindowTraf = new google.maps.InfoWindow({
         content: props.content
       });
     
-      marker.addListener('click', function(){
-        infoWindow.open(map, marker);
+      markerTraf.addListener('click', function(){
+        infoWindowTraf.open(map, markerTraf);
       });
     }
   }
 
   // sets the map on all markers in the array
-  function setMapOnAll(map) {
-    for (let i = 0; i < markersArray.length; i++) {
-      markersArray[i].setMap(map);
+  function setMapOnAllTraf(map) {
+    for (let i = 0; i < markersArrayTraf.length; i++) {
+      markersArrayTraf[i].setMap(map);
     }
   }
 
   // deletes all markers in the array by removing references to them
-  function deleteMarkers() {
-    setMapOnAll(null);
-    markersArray = [];
+  function deleteMarkersTraf() {
+    setMapOnAllTraf(null);
+    markersArrayTraf = [];
   }
 
-
-  let searchBtn = document.getElementById('searchBtn');
-  searchBtn.addEventListener('click', function() {
-    console.log('hi');
-    var input_date = document.getElementById('date').value;;
+  // TRAFFIC API SECTION
+  let searchTraf = document.getElementById('searchTraf');
+  searchTraf.addEventListener('click', function() {
+    console.log('hi traffic');
+    var input_date = document.getElementById('dateTraf').value;;
     console.log(input_date);
   //}, false);
 
     $(document).ready(function(updatedata) {
       //@params settings
-      console.log("ready");
+      console.log("ready traffic");
       var params = {
         // //YYYY-MM-DD[T]HH:mm:ss (SGT)
         "date_time": input_date
@@ -83,9 +86,9 @@ function initMap() {
           {
             var camId = data.items[0].cameras[i].camera_id
             console.log("Camera ID: " + camId);
-            var datalat = data.items[0].cameras[i].location.latitude;
-            var datalng = data.items[0].cameras[i].location.longitude;
-            var dataDT = new Date(data.items[0].cameras[i].timestamp);
+            var datalatTraf = data.items[0].cameras[i].location.latitude;
+            var datalngTraf = data.items[0].cameras[i].location.longitude;
+            var dataTSTraf = new Date(data.items[0].cameras[i].timestamp);
             var image = data.items[0].cameras[i].image;
             console.log(image);
 
@@ -101,9 +104,9 @@ function initMap() {
             });
               
             $("#info").append('<p class="text"><b>Camera ID: </b>' + camId + "</p>");
-            $("#info").append('<p class="text"><b>Latitude: </b>' + datalat + "</p>");
-            $("#info").append('<p class="text"><b>Longitude: </b>' + datalng + "</p>");
-            $("#info").append('<p class="text"><b>Time Stamp: </b>' + dataDT + "</p>");
+            $("#info").append('<p class="text"><b>Latitude: </b>' + datalatTraf + "</p>");
+            $("#info").append('<p class="text"><b>Longitude: </b>' + datalngTraf + "</p>");
+            $("#info").append('<p class="text"><b>Time Stamp: </b>' + dataTSTraf + "</p>");
 
             var img = document.createElement("img");
             img.src = image;
@@ -115,22 +118,22 @@ function initMap() {
 
             document.getElementById("info").id = "infoDone";
 
-            var contentString =
+            var contentStringTraf =
             '<div id="content">' +
             '<div id="siteNotice">' +
             "</div>" +
             '<div id="bodyContent">' +
             "<p>Cam ID: " + camId + 
-            "<p>Latitude: " + datalat + 
-            "<p>Longitude: " + datalng + "</p>" +
+            "<p>Latitude: " + datalatTraf + 
+            "<p>Longitude: " + datalngTraf + "</p>" +
             "</div>" +
             "</div>";
 
             // call function to add marker on map
-            addMarker({
-              coords:{lat: datalat, lng: datalng},
-              iconImage: 'images/camera.png',
-              content: contentString,
+            addMarkerTraf({
+              coords:{lat: datalatTraf, lng: datalngTraf},
+              iconImageTraf: 'images/camera.png',
+              content: contentStringTraf
             });
 
           };
@@ -145,14 +148,137 @@ function initMap() {
     });
   }, false);
 
-  let emptyBtn = document.getElementById('emtpyBtn');
-  emptyBtn.addEventListener('click', function() {
-    console.log('bye');
+  let emtpyTraf = document.getElementById('emtpyTraf');
+  emtpyTraf.addEventListener('click', function() {
+    console.log('bye traffic');
 
     $(document).ready(function(updatedata) {
       $('#td').remove();
       $('#details').empty();
-      deleteMarkers();
+      deleteMarkersTraf();
+    });
+  }, false);
+
+
+
+  // SG WEATHER - RAINFALL
+  // Adds a marker to the map and push to the array
+  function addMarkerRain(props){
+    var markerRain = new google.maps.Marker({
+      position: props.coords,
+      map: map
+    });
+    markersArrayRain.push(markerRain);
+
+    // check for custom icon
+    if (props.iconImageRain){
+      markerRain.setIcon(props.iconImageRain);
+    }
+
+    // check content
+    if (props.content){
+      let infoWindowRain = new google.maps.InfoWindow({
+        content: props.content
+      });
+    
+      markerRain.addListener('click', function(){
+        infoWindowRain.open(map, markerRain);
+      });
+    }
+  }
+
+  // sets the map on all markers in the array
+  function setMapOnAllRain(map) {
+    for (let i = 0; i < markersArrayRain.length; i++) {
+      markersArrayRain[i].setMap(map);
+    }
+  }
+
+  // deletes all markers in the array by removing references to them
+  function deleteMarkersRain() {
+    setMapOnAllRain(null);
+    markersArrayTraf = [];
+  }
+
+  // WEATHER API SECTION
+  let searchRain = document.getElementById('searchRain');
+  searchRain.addEventListener('click', function() {
+    console.log('hi rain');
+    var input_date = document.getElementById('dateRain').value;;
+    console.log(input_date);
+  //}, false);
+
+    $(document).ready(function(updatedata) {
+      //@params settings
+      console.log("ready rain");
+      var params = {
+        // //YYYY-MM-DD[T]HH:mm:ss (SGT)
+        "date_time": input_date
+        };
+      
+        $.ajax({
+        type: "GET",
+        dataType: 'json',
+        contentType: "text/plain",
+        url: "https://api.data.gov.sg/v1/environment/rainfall",
+        headers: {
+      
+        },
+      
+        data: { "date_time": input_date },
+        // crossDomain: true,
+        //@data returning JSON data
+        success: function(data) {
+          console.log(data);
+          
+          for (var i=0; i<(data.metadata.stations).length; i++)
+          {
+            var deviceId = data.metadata.stations[i].device_id;
+            console.log("Device ID: " + deviceId);
+            var datalatRain = data.metadata.stations[i].location.latitude;
+            var datalngRain = data.metadata.stations[i].location.longitude;
+            var dataTSRain = new Date(data.items[0].timestamp);
+            var dataReadingType = data.metadata.reading_type;
+            var dataReadingUnit = data.metadata.reading_unit;
+            var dataValueRain = data.items[0].readings[i].value;
+
+            var contentStringRain =
+            '<div id="content">' +
+            '<div id="siteNotice">' +
+            "</div>" +
+            '<div id="bodyContent">' +
+            "<p>Device ID: " + deviceId +
+            "<p>Latitude: " + datalatRain + 
+            "<p>Longitude: " + datalngRain + 
+            "<p>Rainfall Value: " + dataValueRain + " " + dataReadingUnit + "</p>" +
+            "</div>" +
+            "</div>";
+
+            // call function to add marker on map
+            addMarkerRain({
+              coords:{lat: datalatRain, lng: datalngRain},
+              iconImageRain: 'images/rainfall.png',
+              content: contentStringRain,
+            });
+
+          };
+
+        },
+        // if input is incorrect (wrong format)
+        error: function(data) {
+          console.log("invalid datetime format");
+          alert("Invalid datetime format \nCorrect format: YYYY-MM-DD[T]HH:mm:ss");
+        }
+        })
+    });
+  }, false);
+
+  let emtpyRain = document.getElementById('emtpyRain');
+  emtpyRain.addEventListener('click', function() {
+    console.log('bye rain');
+
+    $(document).ready(function(updatedata) {
+      deleteMarkersRain();
     });
   }, false);
 };
